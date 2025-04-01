3eb25f64e9076f5a2dbb0170558665d59934f116
ABHINASH <abhinashagrawal547@gmail.com>
2025-04-01 18:48:48 -0500

    commiting the latest changes related to pom file

--- src/test/java/stepDefinitions/PracticeSteps.java
+++ src/test/java/stepDefinitions/PracticeSteps.java
@@ -1,11 +1,7 @@
 package stepDefinitions;
 
-import java.util.List;
-
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.en.Given;
